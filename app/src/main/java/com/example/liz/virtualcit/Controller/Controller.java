83e219a209fbeea396ab799ca1ab57a0bbd53553
Eilish <eilish.nithuama@mycit.ie>
2015-03-05 11:29:45 +0000

    yayayayayayayayayayayayay

---
+++ app/src/main/java/com/example/liz/virtualcit/Controller/Controller.java
@@ -0,0 +1,40 @@
+package com.example.liz.virtualcit.Controller;
+
+import com.example.liz.virtualcit.Model.MenuObject;
+
+import java.util.ArrayList;
+
+public class Controller {
+    private static Controller instance;
+    private ArrayList<MenuObject> menuArray = new ArrayList();
+
+    public static Controller getInstance() {
+        if (instance == null) {
+            instance = new Controller();
+        }
+
+        return instance;
+    }
+
+    public ArrayList loadOptions() {
+        MenuObject mo = new MenuObject("MyCit", "http://www.mycit.ie");
+        menuArray.add(mo);
+        mo = new MenuObject("BlackBoard", "http://citbb.blackboard,com");
+        menuArray.add(mo);
+        mo = new MenuObject("Access Student Drive", "http://webvpn.cit.ie/+CSCOE+/logon.html/");
+        menuArray.add(mo);
+        mo = new MenuObject("CIT TimeTables", "http://timetables.cit.ie");
+        menuArray.add(mo);
+
+        return menuArray;
+
+    }
+
+
+    public ArrayList getMenu() {
+        menuArray = loadOptions();
+        return menuArray;
+    }
+
+
+}
